7f0fd56d87d0af6308bd1dea629916601dd075e0
patriano <patryk.dudzik@gmail.com>
2017-11-02 21:02:13 +0100

    Added packeges cotrollers an models Added classes: MainController and Event

--- src/main/java/pl/akademiakodu/setup/models/Event.java
+++ src/main/java/pl/akademiakodu/setup/models/Event.java
@@ -1,7 +1,14 @@
 package pl.akademiakodu.setup.models;
 
+import lombok.Data;
+
 /**
  * Created by Patryk Dudzik on 02.11.2017.
  */
+
+@Data
 public class Event {
+    public String name;
+    public String organizer;
+    public int price;
 }
