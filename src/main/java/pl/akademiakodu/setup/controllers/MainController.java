ebd1a2bc45abedd4ef323f4e216f539859a425de
patriano <patryk.dudzik@gmail.com>
2017-11-03 17:17:31 +0100

    Changes: 1) Added in dependencies servlet api. 2) Added loging system with cookies management 3) Added SessionInfo class to deal with session informations (e.g. user is logged and his username)

--- src/main/java/pl/akademiakodu/setup/controllers/MainController.java
+++ src/main/java/pl/akademiakodu/setup/controllers/MainController.java
@@ -1,12 +1,35 @@
 package pl.akademiakodu.setup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.akademiakodu.setup.models.SessionInfo;
+
+import javax.servlet.http.*;
+import java.util.Arrays;
 
 /**
  * Created by Patryk Dudzik on 02.11.2017.
- */
+         */
 
 @Controller
 public class MainController {
 
+    @Autowired
+    SessionInfo sessionInfo;
+
+    @GetMapping ("/")
+    public String mainPage(HttpServletRequest request, ModelMap modelMap) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            Arrays.stream(cookies).forEach(cookie -> {
+                if (cookie.getName().equals("logingInfo")){
+                    sessionInfo.setLogged(true);
+                    sessionInfo.setUsername(cookie.getValue());
+                }
+            });
+        }
+        return "indexTemplate";
+    }
 }
